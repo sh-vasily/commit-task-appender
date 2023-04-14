652fecac1f685b8dd9053689e223b9dbf2ccab84
Vasily Scherbakov <basil-scherbakov@ya.ru>
2023-04-14 13:58:50 +0300

    add test

--- test_commit_processor.py
+++ test_commit_processor.py
@@ -11,3 +11,9 @@ def test_parse_task_id_complex_message_takes_correct_id():
     task_id = '123'
     commit_message = f'pr/ivanov/{task_id}-task_description'
     assert parse_task_id(commit_message) == task_id
+
+
+def test_parse_task_id_message_with_digits_takes_correct_id():
+    task_id = '123'
+    commit_message = f'pr/ivanov/{task_id}-task_description_kb5'
+    assert parse_task_id(commit_message) == task_id
