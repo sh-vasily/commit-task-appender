45939ceb2b486c84f46498e9fbc23d58ea123812
Vasily Scherbakov <basil-scherbakov@ya.ru>
2023-04-14 13:59:25 +0300

    refactor

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 echo "Installing hook for ${target_repository_dir}"
 
-
 cp -r "$script_dir/hook/commit-msg" "$installation_dir"
 cp -r "$script_dir/hook/commit_processor.py" "installation_dir"
 
