2ea27f6081da0cf6a2c055751d0fb7e0781af59a
shcherbakov.vas <Vasily.Shcherbakov@infotecs.ru>
2023-04-17 15:52:14 +0300

    фикс установщика

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "Installing hook for ${target_repository_dir}"
 
 cp -r "$script_dir/hook/commit-msg" "$installation_dir"
-cp -r "$script_dir/hook/commit_processor.py" "installation_dir"
+cp -r "$script_dir/hook/commit_processor.py" "$installation_dir"
 
 echo "Hook installed in ${target_repository_dir}"
