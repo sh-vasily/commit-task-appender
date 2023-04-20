eeaf88d9a19f2bc8df32900f4967125ad036e148
shcherbakov.vas <Vasily.Shcherbakov@infotecs.ru>
2023-04-20 13:26:25 +0300

    move insertion task number to function

--- hook/commit_processor.py
+++ hook/commit_processor.py
@@ -5,6 +5,10 @@ import re
 def parse_task_id(commit_message):
     return re.findall(r'\d+', commit_message)[0]
 
+def insert_task_number(file_handler, task_id):
+    commit_msg = file_handler.read()
+    file_handler.seek(0, 0)
+    file_handler.write(f'#{task_id} {commit_msg}')
 
 def main():
     message_file = sys.argv[1]
@@ -12,9 +16,7 @@ def main():
 
     task_id = parse_task_id(message_file)
     with open(commit_msg_filepath, 'r+') as fh:
-        commit_msg = fh.read()
-        fh.seek(0, 0)
-        fh.write('#%s %s' % (task_id, commit_msg))
+        insert_task_number(fh, task_id)
     sys.exit(0)
 
 
