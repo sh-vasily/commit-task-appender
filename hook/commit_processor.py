b248bf0bc41960460647c48275fc962bdce67984
shcherbakov.vas <Vasily.Shcherbakov@infotecs.ru>
2023-06-15 18:09:18 +0300

    ignoring branches without task number

--- hook/commit_processor.py
+++ hook/commit_processor.py
@@ -2,23 +2,29 @@ import sys
 import re
 
 
-def parse_task_id(commit_message):
-    return re.findall(r'\d+', commit_message)[0]
+def try_parse_task_id(branch_name):
+    numbers = re.findall(r'\d+', branch_name)
+    return numbers[0] if len(numbers) > 0 else None
 
 def insert_task_number(file_handler, task_id):
     commit_msg = file_handler.read()
     file_handler.seek(0, 0)
     file_handler.write(f'#{task_id} {commit_msg}')
 
-def main():
-    message_file = sys.argv[1]
-    commit_msg_filepath = sys.argv[2]
+def process_commit(branch_name, commit_msg_filepath):
+    task_id = try_parse_task_id(branch_name)
+    if task_id is None:
+        return
 
-    task_id = parse_task_id(message_file)
     with open(commit_msg_filepath, 'r+') as fh:
         insert_task_number(fh, task_id)
-    sys.exit(0)
 
+def main():
+    branch_name = sys.argv[1]
+    commit_msg_filepath = sys.argv[2]
+
+    process_commit(branch_name, commit_msg_filepath)
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
--- test_commit_processor.py
+++ test_commit_processor.py
@@ -1,23 +1,23 @@
-from hook.commit_processor import parse_task_id, insert_task_number
+from hook.commit_processor import try_parse_task_id, insert_task_number, process_commit 
 import tempfile
 
 
 def test_parse_task_id_takes_correct_id():
     task_id = '123'
     branch = f'pr/ivanov/{task_id}'
-    assert parse_task_id(branch) == task_id
+    assert try_parse_task_id(branch) == task_id
 
 
 def test_parse_task_id_complex_message_takes_correct_id():
     task_id = '123'
     branch = f'pr/ivanov/{task_id}-task_description'
-    assert parse_task_id(branch) == task_id
+    assert try_parse_task_id(branch) == task_id
 
 
 def test_parse_task_id_message_with_digits_takes_correct_id():
     task_id = '123'
     branch = f'pr/ivanov/{task_id}-task_description_kb5'
-    assert parse_task_id(branch) == task_id
+    assert try_parse_task_id(branch) == task_id
 
 
 def test_insert_task_number_unicode_message():
@@ -35,3 +35,19 @@ def test_insert_task_number_unicode_message():
 
         #assert
         assert processed_message == f'#{task_number} {original_message}'
+
+
+def test_nothing_inserted_when_branch_name_hasnt_task_number():
+    with tempfile.NamedTemporaryFile('r+') as tmp:
+        #arrange
+        original_message = 'Сообщение'
+        tmp.write(original_message)
+        tmp.seek(0, 0)
+
+        #act
+        process_commit("pr/ivanov/fake", tmp)
+        tmp.seek(0, 0)
+        processed_message = tmp.read()
+
+        #assert
+        assert processed_message == original_message
