c25f6857dd692dc6280040cedb66b9cb30d1c8f5
Василий Щербаков <basil-scherbakov@ya.ru>
2024-02-03 00:20:10 +0300

    [new-strategies] strategies support

--- install.sh
+++ install.sh
@@ -9,4 +9,11 @@ echo "Installing hook for ${target_repository_dir}"
 cp -r "$script_dir/hook/commit-msg" "$installation_dir"
 cp -r "$script_dir/hook/commit_processor.py" "$installation_dir"
 
-echo "Hook installed in ${target_repository_dir}"
+strategy="insert_branch_name"
+
+if [ ! -z "$2" ]; then
+    strategy="$2"
+    echo "${strategy}" > "$installation_dir/proccessor.conf"
+fi
+
+echo "Hook installed in ${target_repository_dir}. Strategy - ${strategy} will be used."
